bf773fc515eb01d457cc86e311b4558a8711d10c
Gabriele Ventura <gabriele.ventura.stud@ispascalcomandini.it>
2024-10-14 09:05:21 +0200

    Release 0.0.1

--- EsercitazioneCD/EsercitazioneCD/CD.cs
+++ EsercitazioneCD/EsercitazioneCD/CD.cs
@@ -8,5 +8,55 @@ namespace EsercitazioneCD
 {
     internal class CD
     {
+        private string titolo;
+        private string autore;
+        private List<Brano> brani;
+
+        public CD(string titolo, string autore, List<Brano> brani)
+        {
+            this.titolo = titolo;
+            this.autore = autore;
+            this.brani = brani;
+        }
+
+        public string getTitolo()
+        {
+            return titolo;
+        }
+
+        public void setTitolo(string titolo)
+        {
+            this.titolo = titolo;
+        }
+
+        public string getAutore()
+        {
+            return autore;
+        }
+
+        public void setAutore(string autore)
+        {
+            this.autore = autore;
+        }
+
+        public string toString()
+        {
+            string s = "";
+            foreach (Brano b in brani)
+            {
+                s += b.getTitolo() + "\t";
+            }
+            return s;
+        }
+
+        public double getDurata()
+        {
+            double durata = 0;
+            foreach (Brano b in brani)
+            {
+                durata += b.getDurata();
+            }
+            return durata;
+        }
     }
 }
