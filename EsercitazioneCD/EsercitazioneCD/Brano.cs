79b27bb66167e6c0205ad00ae3510bcf1814aa8b
Elia Lanzoni <ELIA.LANZONI@local.ittcesena.it>
2024-10-14 09:04:34 +0200

    Release 0.0.1

--- EsercitazioneCD/EsercitazioneCD/Brano.cs
+++ EsercitazioneCD/EsercitazioneCD/Brano.cs
@@ -8,5 +8,61 @@ namespace EsercitazioneCD
 {
     internal class Brano
     {
+        // Attributi privati
+        private string titolo;
+        private string autore;
+        private double durata;
+
+        // Costruttore
+        public Brano(string titolo, string autore, double durata)
+        {
+            this.titolo = titolo;
+            this.autore = autore;
+            this.durata = durata;
+        }
+
+        // Metodi getter
+        public string GetTitolo()
+        {
+            return titolo;
+        }
+
+        public string GetAutore()
+        {
+            return autore;
+        }
+
+        public double GetDurata()
+        {
+            return durata;
+        }
+
+        // Metodi setter
+        public void SetTitolo(string titolo)
+        {
+            this.titolo = titolo;
+        }
+
+        public void SetAutore(string autore)
+        {
+            this.autore = autore;
+        }
+
+        public void SetDurata(double durata)
+        {
+            this.durata = durata;
+        }
+
+        // Metodo ToString che restituisce una stringa con tutti i dati dell'oggetto
+        public override string ToString()
+        {
+            return $"Titolo: {titolo}, Autore: {autore}, Durata: {durata} minuti";
+        }
+
+        // Metodo shortSong che verifica se il brano ha una durata inferiore al limite passato
+        public bool ShortSong(double durataLimite)
+        {
+            return durata < durataLimite;
+        }
     }
 }
